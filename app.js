60d3e37a08ecc6b2b30ab5ed9bd321011166cdab
Casandra Silva <silvacasandra@gmail.com>
2016-01-26 13:14:02 -0800

    prevents user from submitting blank posts

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ app.controller('MainCtrl',['$scope',
       {title: 'post 5', upvotes: 4}
     ];
     $scope.addPost = function(){
+      if (!$scope.title || $scope.title === '') {
+        return; }
       $scope.posts.push({title: $scope.title, upvotes: 0});
       $scope.title = '';
     }
   }
 ])
-.filter('order', OrderFilter);
